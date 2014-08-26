8280948e1ead00fa560591c2bf1394d1798a9b8b
jonothon.pollard <jonothon.pollard@MU-C3-15.mullen.pvt.k12.co.us>
2014-08-26 08:26:13 -0600

    Added School

--- Fork Tester/src/ForkTester.java
+++ Fork Tester/src/ForkTester.java
@@ -3,6 +3,7 @@ public class ForkTester
 
 	public static void main(String[] args)
 		{
+		System.out.println("School.");
 		// Try what you did before.  
 		// Fork this to your personal GitHub account.
 		// Download it to school and add a line.
